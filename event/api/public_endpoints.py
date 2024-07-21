bdcf20baf73d4d76f4d6288f32ca43b9905c3eb2
Sahil-Mandaliya <sahilmandaliya9967@gmail.com>
2024-07-22 03:36:03 +0530

    update changes

--- event/api/public_endpoints.py
+++ event/api/public_endpoints.py
@@ -16,7 +16,6 @@ def get_all_events_api(db: Session = Depends(get_db)):
     except HTTPException as err:
         raise
     except Exception as err:
-        raise
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Something went wrong!")
     # Add rollback in exceptions
 
