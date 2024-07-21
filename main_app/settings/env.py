8792da56fe05de5401ebb63beda2c9730a416a95
Sahil-Mandaliya <sahilmandaliya9967@gmail.com>
2024-07-21 09:44:09 +0530

    env change

---
+++ main_app/settings/env.py
@@ -0,0 +1,10 @@
+from os import environ
+import json
+from alembic import context
+from sqlalchemy import engine_from_config, pool
+
+from dotenv import load_dotenv
+
+DATABASE_URL = environ.get("DATABASE_URL")
+config = context.config
+config.set_main_option('sqlalchemy.url', DATABASE_URL)
